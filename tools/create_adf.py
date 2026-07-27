2de376ee95c480dc30aef819f608a8f6673dfe37
Anupama Rajendra <asrajendra@wisc.edu>
2026-07-27 10:48:14 -0500

    make adfs from dictionary

--- tools/create_adf.py
+++ tools/create_adf.py
@@ -7,3 +7,10 @@ def generate_adf(run_lbl, output_filepath):
     adf = alara_data.extract_tables()
     return adf
 
+def generate_adfs_from_dict(run_dict):
+    # generating multiple adfs as adf_to_sqlite.py is set up to work with one adf at a time
+    adf_list = []
+    for run_lbl, output_filepath in run_dict.items():
+        adf = generate_adf(run_lbl, output_filepath)
+        adf_list.append(adf)
+    return adf_list    
